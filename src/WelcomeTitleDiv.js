1d445a22f55df756bfc895f22b09fd7feae7eb05
VISHWANATH <71350788+VISHWANATH987@users.noreply.github.com>
2022-02-26 22:16:35 +0530

    Update WelcomeTitleDiv.js

--- src/WelcomeTitleDiv.js
+++ src/WelcomeTitleDiv.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 function WelcomeTitleDiv() {
   return <div className="WelcomeTitleDiv">
       <div className='WelcomeTitleDivContent'>KLS HOSPITAL MANAGEMENT SYSTEM</div>
